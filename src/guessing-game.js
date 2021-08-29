77e5a20af1755f018d6a189724aeae3807dd7042
mcPiper <66990552+mcPiper@users.noreply.github.com>
2021-08-29 19:25:49 +0300

    init: update code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max;
     }
 
     guess() {
-
+        this._guess = Math.ceil((this._max - this._min) / 2) + this._min;
+        return this._guess;
     }
 
     lower() {
-
+        this.setRange(this._min, this._guess);
+        this.guess();
     }
 
     greater() {
-
+        this.setRange(this._guess, this._max);
+        this.guess();
     }
 }
 
